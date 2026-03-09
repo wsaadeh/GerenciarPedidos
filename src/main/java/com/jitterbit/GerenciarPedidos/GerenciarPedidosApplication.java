12210e7b5ec88df191b41d37c4989fed313d333d
Wilson Saadeh <wilson_saadeh@hotmail.com>
2026-03-09 15:25:23 -0300

    project creation

---
+++ src/main/java/com/jitterbit/GerenciarPedidos/GerenciarPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.jitterbit.GerenciarPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jitterbit/GerenciarPedidos/GerenciarPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitterbit.GerenciarPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
